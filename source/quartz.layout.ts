4191197be686cd37f16d075b1b70e6d0d2353836
Thomas Knudson <159010282+quantumrook@users.noreply.github.com>
2025-01-04 11:56:51 -0800

    Update quartz.layout.ts

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -5,11 +5,24 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'quantumrook/quantumrook.github.io',
+        // from data-repo-id
+        repoId: 'R_kgDONk_yTw',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDONk_yT84CluRn',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/quantumrook/",
     },
   }),
 }
