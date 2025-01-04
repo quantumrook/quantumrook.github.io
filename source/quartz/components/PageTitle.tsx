ea1f05ff51159974ddf486f2516fe82410583b33
Thomas Knudson <159010282+quantumrook@users.noreply.github.com>
2025-01-04 13:25:27 -0800

    Update PageTitle.tsx

--- source/quartz/components/PageTitle.tsx
+++ source/quartz/components/PageTitle.tsx
@@ -7,6 +7,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
+    <link rel="icon" href={iconPath} />
     <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
     </h2>
